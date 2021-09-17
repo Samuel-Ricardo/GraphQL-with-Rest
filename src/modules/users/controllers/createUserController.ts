a551a3223d779e09a8d0303ce12060d693d24e74
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-17 00:33:02 -0300

    Create: Controller - CreateUser (src/modules/users/controllers)

--- src/modules/users/controllers/createUserController.ts
+++ src/modules/users/controllers/createUserController.ts
@@ -1,11 +1,21 @@
+import { container } from "tsyringe";
+import { CreateUserService } from "../services/CreateUserService";
 
 
 class CreateUserController {
   async handle(request: Request, response: Response) {
 
-    //const { name, userName } = request.body;
+    const { name, userName } = request.body;
 
-   // const CreateUserService = container.resolver()
+    const createUserService = container.resolve(CreateUserService);
 
+    const user = await CreateUserService.execute({
+      name,
+      userName,
+    });
+
+    return response.json(user);
   }
 }
+
+export { CreateUserController };
