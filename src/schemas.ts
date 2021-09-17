557c77ea528968e0663c8fc919a7a507e421e057
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-17 01:26:07 -0300

    Create: index - resolvers (src)

--- src/schemas.ts
+++ src/schemas.ts
@@ -3,7 +3,7 @@ import { mergeResolvers, mergeTypeDefs } from "@graphql-tools/merge";
 import path from "path";
 
 const mergePath = loadFilesSync(
-  path.join(__dirname, "modules/**/graphql/*.gql");
+  path.join(__dirname, "modules/**/graphql/*.gql")
 );
 
 const schemas = mergeTypeDefs(mergePath);
