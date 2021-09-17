6d1ec67f5d9d167daf9f998e1dbb7ce5ecf7a316
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-16 23:45:07 -0300

    create: server - localhost:4003 (src/server)

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,16 @@
 import mongoose from "mongoose";
+import express, { NextFunction, Request, Response } from "express";
 
 
 mongoose.connect("mongodb//localhost:27017/easy_posts", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+const app = express();
+
+app.use(express.json());
+
+//app.use(routes);
+
+app.listen(4003, () => console.log("server is Running: http://localhost:4003/"));
