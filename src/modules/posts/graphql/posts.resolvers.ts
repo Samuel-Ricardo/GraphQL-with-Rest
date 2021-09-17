77e8b0523f8a3706759a2a1802c113656eb0ae49
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-17 01:14:29 -0300

    Create: Resolver - postResolvers (src/modules/posts/graphql)

--- src/modules/posts/graphql/posts.resolvers.ts
+++ src/modules/posts/graphql/posts.resolvers.ts
@@ -1,11 +1,26 @@
 import { container } from "tsyringe";
+import { CreatePostService } from "../services/CreatePostService";
+import { GetPostByUserService } from "../services/GetPostByUserService";
 
 
 
 const postResolvers = {
+
   Mutation: {
     createPost(_, { input }) {
-      const createPostService = container.resolve(CreatePost)
-    }
-  }
-}
+      const createPostService = container.resolve(CreatePostService);
+      const post = createPostService.execute(input);
+      return post;
+    },
+  },
+
+  Query: {
+    getPostByUser(context, { idUser }) {
+      const getPostByUserService = container.resolve(GetPostByUserService);
+      const posts = getPostByUserService.excute(idUser);
+      return posts;
+    },
+  },
+};
+
+export default postResolvers
