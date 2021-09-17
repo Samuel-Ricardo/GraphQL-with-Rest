caf14bc665b7e97cd507e0d951ef881bd2c89d75
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-17 01:01:48 -0300

    Create Class Controller - GetPostByUser (src/modules/posts/controllers)

--- src/modules/posts/controllers/GetPostsByUserController.ts
+++ src/modules/posts/controllers/GetPostsByUserController.ts
@@ -1,5 +1,16 @@
+import { container } from "tsyringe";
+import { GetPostByUserService } from "../services/GetPostByUserService";
 
 
 class GetPostByUserController {
-  
+  async handle(request Request, response: Response) {
+
+    const { id } = request.params;
+
+    const getPostByUserController = container.resolve(GetPostByUserService);
+    const post = await getPostByUserService.execute(id);
+    return response.json(posts);
+  }
 }
+
+export { GetPostByUserController };
