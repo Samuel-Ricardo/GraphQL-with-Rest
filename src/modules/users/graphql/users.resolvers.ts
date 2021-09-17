b7e9bde51fefa418f6028f8dd44bf002c306f809
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-17 00:49:06 -0300

    Create: Resolver - User (src/modules/users/graphql)

---
+++ src/modules/users/graphql/users.resolvers.ts
@@ -0,0 +1,21 @@
+import { container } from "tsyringe"
+import { CreateUserService } from "../services/CreateUserService";
+import { GetUsersService } from "../services/GetUsersService"
+
+
+const userResolvers = {
+
+  Query: {
+    getAllUsers() {
+      const getUsersServices = container.resolve(GetUsersService);
+      const users = getUsersServices.execute();
+      return users;
+    },
+  },
+
+  Mutation: {
+    CreateUser(context, { input }) { return container.resolve(CreateUserService).execute(input) }
+  },
+};
+
+export default userResolvers;
