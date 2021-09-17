46ce3983775561765119ed311807498f58bb6bc4
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-17 00:28:10 -0300

    Create: Service - GetUsers (src/modules/users/services)

---
+++ src/modules/users/services/GetUsersService.ts
@@ -0,0 +1,13 @@
+import { User } from "@models/User";
+import { injectable } from "tsyringe";
+
+
+@injectable()
+class GetUsersService {
+  async execute() {
+    const users = await User.find();
+    return users;
+  }
+}
+
+export { GetUsersService };
